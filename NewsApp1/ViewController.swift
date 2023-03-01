ef8d135b8bf14b815b1aea897b003614e12ca8fa
daniel jermaine <danieljermaine2@gmail.com>
2023-03-01 09:06:52 +0100

    Initial Commit

---
+++ NewsApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp1
+//
+//  Created by Apple on 01/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
